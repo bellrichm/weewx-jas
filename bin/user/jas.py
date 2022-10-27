84544f7a2c0f5d48218d3e8d91a83870c0f4cb43
Rich Bell <bellrichm@gmail.com>
2022-10-27 13:13:58 -0400

    up version indicator

--- bin/user/jas.py
+++ bin/user/jas.py
@@ -155,7 +155,7 @@ except ImportError:
         logmsg(syslog.LOG_ERR, msg)
 
 
-VERSION = "0.2.4-rc01"
+VERSION = "0.2.4-rc02"
 
 class JAS(SearchList):
     """ Implement tags used by templates in the skin. """
--- install.py
+++ install.py
@@ -12,7 +12,7 @@ except ImportError:
 import configobj
 from weecfg.extension import ExtensionInstaller
 
-VERSION = "0.2.4-rc01"
+VERSION = "0.2.4-rc02"
 
 EXTENSION_CONFIG = """
 [StdReport]
