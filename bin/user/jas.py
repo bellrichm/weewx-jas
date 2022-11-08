d3dc0412b847fd36096289865c3d56d935fd9015
Rich Bell <bellrichm@gmail.com>
2022-11-08 10:23:28 -0500

    easier to save to file

--- bin/user/jas.py
+++ bin/user/jas.py
@@ -167,7 +167,7 @@ except ImportError:
         logmsg(syslog.LOG_ERR, msg)
 
 
-VERSION = "0.2.4-z05"
+VERSION = "0.2.4-z06"
 
 class JAS(SearchList):
     """ Implement tags used by templates in the skin. """
--- install.py
+++ install.py
@@ -12,7 +12,7 @@ except ImportError:
 import configobj
 from weecfg.extension import ExtensionInstaller
 
-VERSION = "0.2.4-z05"
+VERSION = "0.2.4-z06"
 
 EXTENSION_CONFIG = """
 [StdReport]
