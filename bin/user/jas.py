26142b43510043b995d845413bd711689807d0e0
Rich Bell <bellrichm@gmail.com>
2023-08-30 17:28:31 -0400

    Query strings to manage cache (#124)
    * post release
    
    * cache busting experiment
    
    * cache
    
    * configure query string on data request
    
    * configure when to add query string
    
    * up version

--- bin/user/jas.py
+++ bin/user/jas.py
@@ -176,7 +176,7 @@ except ImportError:
         logmsg(syslog.LOG_ERR, msg)
 
 
-VERSION = "1.0.1-rc01"
+VERSION = "1.1.0-rc01"
 
 class JAS(SearchList):
     """ Implement tags used by templates in the skin. """
